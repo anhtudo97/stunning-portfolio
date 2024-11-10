b3b76e1869c7656fa6e6b20264f62379cec788c4
anhtudo97 <anhtudo97@gmail.com>
2024-11-10 23:51:33 +0700

    feat: init project

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return <div>
+    <div>
+      <nav>
+        <a href="#" id="home">Home</a>
+        <a href="#" id="projects">Projects</a>
+        <a href="#" id="about">About</a>
+        <a href="#" id="contact">Contact</a>
+      </nav>
+    </div>
+  </div>;
+};

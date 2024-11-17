91446cc8aa11a763943f67eaabc68b4d25c73c9a
anhtudo97 <anhtudo97@gmail.com>
2024-11-17 22:57:32 +0700

    feat: contact sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -13,7 +14,8 @@ export default function Home() {
       <ProjectsSection />
       <TapeSection />
       <TestimonialsSection />
-      <AboutSection /> 
+      <AboutSection />
+      <ContactSection />
     </>
   );
 }
